f81098dd84543efab437988d954c041e7f3a39f2
이승민 <min429@omsecurity.kr>
2025-10-01 13:58:25 +0900

    Initial commit

---
+++ src/main/kotlin/org/example/webfluxstudy/WebFluxStudyApplication.kt
@@ -0,0 +1,11 @@
+package org.example.webfluxstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebFluxStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebFluxStudyApplication>(*args)
+}
