3b3271237c730ff411ed2daf333c7b78c59668ab
이승민 <min429@omsecurity.kr>
2025-10-14 17:00:31 +0900

    flatMap, map

--- src/test/kotlin/org/example/webfluxstudy/KotlinTest.kt
+++ src/test/kotlin/org/example/webfluxstudy/KotlinTest.kt
@@ -124,4 +124,51 @@ class KotlinTest {
             // 어짜피 데이터를 받아서 처리하고 넘기고 나서 다음 데이터를 처리하는 구조라 의미 없음
         }
     }
+
+    @Nested
+    inner class MonoTest {
+
+        @Test
+        @DisplayName("just vs fromCallable vs defer")
+        fun test1() {
+            // value initialize
+            var value = "just"
+            println("initial value: $value")
+
+            // just
+            val just = Mono.just(value)
+
+            // fromCallable
+            val fromCallable = Mono.fromCallable {
+                value
+            }
+
+            // defer
+            val defer = Mono.defer {
+                Mono.just(value)
+            }
+
+            println("================================================")
+            just.subscribe { println("just value: $it") }
+            println("================================================")
+            fromCallable.subscribe { println("fromCallable value1: $it") }
+
+            // value reinitialize
+            value = "fromCallable"
+
+            fromCallable.subscribe { println("fromCallable value2: $it") }
+            println("================================================")
+            defer.subscribe { println("defer value1: $it") }
+
+            // value reinitialize
+            value = "defer"
+
+            defer.subscribe { println("defer value2: $it") }
+
+            // just: 체인 형성 도중에 즉시 값 생성,
+            // fromCallable: subscribe 시 값 생성,
+            // defer: subscribe 시 내부 Publisher 생성 후 바로 subscribe (값 생성)
+            // fromCallable, defer는 반환 형식만 다르고 동일한 기능으로 예상됨
+        }
+    }
 }
